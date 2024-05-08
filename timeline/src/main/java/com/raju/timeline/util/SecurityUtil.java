dbbfee9f2c29ca74c9f10da952004f818dd4e991
duck_ach <djagmlfk@naver.com>
2024-05-09 00:18:16 +0900

    side 주제 변경 및 sql 등록

---
+++ timeline/src/main/java/com/raju/timeline/util/SecurityUtil.java
@@ -0,0 +1,14 @@
+package com.raju.timeline.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtil {
+    public String preventXSS(String str) {
+        str = str.replace("<", "&lt;");
+        str = str.replace(">", "&gt;");
+        str = str.replace("\"", "&quot;");
+        str = str.replace("\'", "&#x27;");
+        return str;
+    }
+}
