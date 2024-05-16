1f65ff46df9d7e378375927c45a57d4f53035b21
duck_ach <djagmlfk@naver.com>
2024-05-17 00:25:10 +0900

    유머게시판 프레임워크 구성 및 임시저장용 SQL 생성

---
+++ timeline/src/main/java/com/raju/timeline/service/HumorBoardService.java
@@ -0,0 +1,8 @@
+package com.raju.timeline.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface HumorBoardService {
+    public void addHumorBoard(HttpServletRequest request, HttpServletResponse response);
+}
